5e9c5a6b1fae3ec199d9bf44a9774d8615656f89
mmakiri <miska.makirinta@edu.turkuamk.fi>
2020-12-14 03:20:42 +0200

    Basic webapp structure

--- src/index.js
+++ src/index.js
@@ -4,14 +4,47 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class MyForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <h1>Maalilaskuri</h1>
+        <p>Maalin nimi:</p>
+        <input
+          type="text"
+        />
+
+        <p>hinta (€/L)</p>
+        <input
+          type="text"
+        />
+
+        <p>Riittoisuus (m2/L)</p>
+        <input
+          type="text"
+        />
+
+        <p>Maalauskerrat</p>
+        <input
+          type="text"
+        />
+        <p></p>
+        <input
+          type='submit'
+        />
+
+        <h1>TO DO:</h1>
+        <p>CSS tyylitys</p>
+        <p>Aktuaalinen funktionaalisuus</p>
+        <p>Laskuri joka ottaa arvot formista ja sitten näyttää tulokset</p>
+
+      </form>
+    );
+  }
+}
+ReactDOM.render(<MyForm />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
